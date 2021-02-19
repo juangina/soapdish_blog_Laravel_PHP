d67a2747a43bc63264057e68b6f23d75b38b89d5
Juan Johnson <ericrenee21@gmail.com>
2021-02-19 00:24:32 -0500

    Updated Migrations

--- database/migrations/2021_02_13_022012_create_posts_table.php
+++ database/migrations/2021_02_13_022012_create_posts_table.php
@@ -14,10 +14,11 @@ class CreatePostsTable extends Migration
     public function up()
     {
         Schema::create('posts', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
             $table->string('title');
             $table->mediumText('body');
             $table->timestamps();
+            $table->integer('user_id');
         });
     }
 
