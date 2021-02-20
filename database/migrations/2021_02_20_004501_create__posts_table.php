0376bf055cfb51a4bac47262dc07d8def0b9f451
Juan Johnson <ericrenee21@gmail.com>
2021-02-19 19:53:38 -0500

    Revise Post Table

---
+++ database/migrations/2021_02_20_004501_create__posts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('_posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->mediumText('body');
+            $table->string('cover_image', 100)->nullable();
+            $table->timestamps();
+            $table->integer('user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('_posts');
+    }
+}
